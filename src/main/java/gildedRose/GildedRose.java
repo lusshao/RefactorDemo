081e97a87a5ed3df5166fe1b2b848a94ea544814
Liam Lu <luanyuanshizhu@126.com>
2019-08-19 21:46:02 +0800

    abstract some common method

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -18,33 +18,33 @@ public class GildedRose {
         switch (item.name) {
             case "Aged Brie":
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.setQuality(item.getQuality() + 1);
                 }
                 item.sellIn = item.sellIn - 1;
                 if (item.sellIn < 0) {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                        item.setQuality(item.getQuality() + 1);
                     }
                 }
                 return;
             case "Backstage passes to a TAFKAL80ETC concert":
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.setQuality(item.getQuality() + 1);
                     if (item.sellIn < 11) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                            item.setQuality(item.getQuality() + 1);
                         }
                     }
 
                     if (item.sellIn < 6) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                            item.setQuality(item.getQuality() + 1);
                         }
                     }
                 }
                 item.sellIn = item.sellIn - 1;
                 if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality;
+                    item.setQuality(0);
                 }
                 return;
 
@@ -52,12 +52,12 @@ public class GildedRose {
                 return;
             default:
                 if (item.quality > 0) {
-                    item.quality = item.quality - 1;
+                    item.setQuality(item.getQuality() - 1);
                 }
                 item.sellIn = item.sellIn - 1;
                 if (item.sellIn < 0) {
                     if (item.quality > 0) {
-                        item.quality = item.quality - 1;
+                        item.setQuality(item.getQuality() - 1);
                     }
                 }
                 return;
--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -18,4 +18,28 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
 }
