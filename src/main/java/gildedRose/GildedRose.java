3ddb55d2d930b4b8e7b23772aa412c2c454a63b8
Liam Lu <luanyuanshizhu@126.com>
2019-08-19 21:21:29 +0800

    abstract Backstage passes to a TAFKAL80ETC concert case

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -26,6 +26,26 @@ public class GildedRose {
                     }
                 }
                 return;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality;
+                }
+                return;
 
         }
         if (!item.name.equals("Aged Brie")
